70c7758e88eb7f329486bb4915c7f32ba4c8602e
mechanicworld <47930174+mechanicworld@users.noreply.github.com>
2021-05-18 01:13:09 +0300

    Initialize Structure and Components

---
+++ src/components/Buttons/Buttons.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+import style from './Buttons.module.css'
+
+function Buttons() {
+  return (
+    <div>
+      <Col>
+        <Row>
+          <Button className={style.bookmarks} >Bookmarks</Button>
+          <Button className={style.allDocs} >All Docs</Button>
+        </Row>
+      </Col>
+    </div>
+  )
+}
+
+export default Buttons
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ApiCard from '../components/ApiCard/ApiCard'
+
+function Dashboard() {
+  return (
+    <div>
+      <ApiCard/>
+    </div>
+  )
+}
+
+export default Dashboard
